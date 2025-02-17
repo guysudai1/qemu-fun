9a837fd5b0b0fb9f90a1c6704b0be48767ff910d
guysu <sudaiguy1@gmail.com>
2025-02-18 01:35:36 +0200

    Implement USART1 Device

---
+++ hw/char/stm32l152_usart.c
@@ -0,0 +1,225 @@
+
+#include "qemu/osdep.h"
+#include "hw/qdev-core.h"
+#include "exec/memory.h"
+#include "hw/char/stm32l152_usart.h"
+#include "qemu/qemu-print.h"
+#include "hw/arm/stm32l152_soc.h"
+#include "hw/registerfields.h"
+#include <stdint.h>
+#include "hw/qdev-properties.h"
+#include "hw/qdev-properties-system.h"
+#include "qapi/error.h"
+#include "sysemu/sysemu.h"
+#include "exec/address-spaces.h"
+#include "exec/cpu-common.h"
+
+uint64_t stm32l152_usart1_read(void *opaque, hwaddr addr, unsigned size);
+void stm32l152_usart1_write(void *opaque, hwaddr addr, uint64_t data, unsigned size);
+
+REG32(SR, 0x00)
+    FIELD(SR, CTS, 9, 1)    /* CTS Flag */
+    FIELD(SR, LBD, 8, 1)    /* LIN break detection flag */
+    FIELD(SR, TXE, 7, 1)    /* Transmit data register empty */
+    FIELD(SR, TC, 6, 5)     /* Transmission complete */
+    FIELD(SR, RXNE, 5, 5)   /* Read data register not empty */
+    FIELD(SR, IDLE, 4, 1)   /* IDLE line detected */
+    FIELD(SR, ORE, 3, 1)    /* Overrun error */
+    FIELD(SR, NF, 2, 1)     /* Noise detected flag */
+    FIELD(SR, FE, 1, 1)     /* Framing error */
+    FIELD(SR, PE, 0, 1)     /* Parity error */
+REG32(DR, 0x04)
+    FIELD(DR, DR, 0, 9)     /* Data value */
+REG32(BRR, 0x08)
+    FIELD(BRR, Fraction, 4, 12)     /* mantissa of USARTDIV */
+    FIELD(BRR, Mantissa, 0, 4)      /* fraction of USARTDIV */
+REG32(CR1, 0x0C)
+    FIELD(CR1, OVER8, 15, 1) /* Oversampling mode */
+    FIELD(CR1, UE, 13, 1)   /* USART enable */
+    FIELD(CR1, M0, 12, 1)    /* Word length (part 1, see M1) */
+    FIELD(CR1, WAKE, 11, 1)  /* Receiver wakeup method */
+    FIELD(CR1, PCE, 10, 1)   /* Parity control enable */
+    FIELD(CR1, PS, 9, 1)     /* Parity selection */
+    FIELD(CR1, PEIE, 8, 1)   /* PE interrupt enable */
+    FIELD(CR1, TXEIE, 7, 1)  /* TXE interrupt enable */
+    FIELD(CR1, TCIE, 6, 1)   /* Transmission complete interrupt enable */
+    FIELD(CR1, RXNEIE, 5, 1) /* RXNE interrupt enable */
+    FIELD(CR1, IDLEIE, 4, 1) /* IDLE interrupt enable */
+    FIELD(CR1, TE, 3, 1)     /* Transmitter enable */
+    FIELD(CR1, RE, 2, 1)     /* Receiver enable */
+    FIELD(CR1, RWU, 1, 1)   /* Receiver wakeup */
+    FIELD(CR1, SBK, 0, 1)   /* Send break */
+
+/* Called whenever someone reads from the USART1 MMIO region*/
+uint64_t stm32l152_usart1_read(void *opaque, hwaddr addr, unsigned size) {
+    Stm32l152Usart1State* rc = STM32L152_USART1(opaque);
+    uint64_t return_val = 0;
+
+    if (size != 4) {
+        qemu_printf("Bad size! Expcted 4 received size=%u\n", size);
+        return 0;
+    }
+
+    switch (addr) {
+        case 0x0: {
+            return_val = rc->state.sr;
+            break;
+        }
+        case 0x4: {
+            return_val = rc->state.dr;
+            break;
+        }
+        case 0x8: {
+            return_val = rc->state.brr;
+            break;
+        }
+        case 0xc: {
+            return_val = rc->state.cr1;
+            break;
+        }
+        case 0x10: {
+            return_val = rc->state.cr2;
+            break;
+        }
+        case 0x14: {
+            return_val = rc->state.cr3;
+            break;
+        }
+        case 0x18: {
+            return_val = rc->state.gtpr;
+            break;
+        }
+        default: {
+            qemu_printf("Bad offset! received offset=0x%" PRIx64 "\n", addr);
+            return 0;
+        }
+    }
+
+    return return_val;
+}
+
+/* Called whenever someone writes to the USART1 MMIO region*/
+void stm32l152_usart1_write(void *opaque, hwaddr addr, uint64_t data, unsigned size) {
+    Stm32l152Usart1State* rc = STM32L152_USART1(opaque);
+
+    if (size != 4) {
+        qemu_printf("Bad size! Expcted 4 received size=%u\n", size);
+        return;
+    }
+
+    switch (addr) {
+        case 0x0: {
+            rc->state.sr = data;
+            break;
+        }
+        case 0x4: {
+            rc->state.dr = data;
+            if (FIELD_EX32(rc->state.sr, SR, TXE)) {
+                rc->state.sr = FIELD_DP32(rc->state.sr, SR, TXE, 0);
+            }
+            // Send the data here and turn on TXE after finishing
+            uint8_t byte_to_send = (rc->state.dr & 0xFF);
+            qemu_chr_fe_write_all(&rc->char_backend, (const uint8_t*)&byte_to_send, sizeof(byte_to_send));
+            rc->state.sr = FIELD_DP32(rc->state.sr, SR, TXE, 1);
+            break;
+        }
+        case 0x8: {
+            rc->state.brr = data;
+            break;
+        }
+        case 0xc: {
+            rc->state.cr1 = data;
+            if (FIELD_EX32(data, CR1, UE)) {
+                // Turn on TXE
+                rc->state.sr = FIELD_DP32(rc->state.sr, SR, TXE, 1);
+            }
+            break;
+        }
+        case 0x10: {
+            rc->state.cr2 = data;
+            break;
+        }
+        case 0x14: {
+            rc->state.cr3 = data;
+            break;
+        }
+        case 0x18: {
+            rc->state.gtpr = data;
+            break;
+        }
+        default: {
+            qemu_printf("Bad offset! received offset=0x%" PRIx64 "\n", addr);
+            return;
+        }
+    }
+}
+
+static MemoryRegionOps usart1_mops = {
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .read = &stm32l152_usart1_read,
+    .write = &stm32l152_usart1_write,
+};
+
+
+static int stm32l152_usart1_can_receive(void *opaque) {
+    // TODO: Add logic when we can't really receive bytes
+    return 1;
+}
+
+static void stm32l152_usart1_receive(void *opaque, const uint8_t *buf, int size) {
+    // TODO: Receive bytes here
+    qemu_printf("Received byte at USART1. buf=0x%p (size=%u). Byte=%c\n", buf, size, buf[0]);
+    Stm32l152Usart1State* rc = STM32L152_USART1(opaque);
+    (void)rc;
+
+}
+
+static void stm32l152_usart1_init(Object *stm32l152_obj) {
+    Stm32l152Usart1State* rc = STM32L152_USART1(stm32l152_obj);
+
+    /* Connect chardev serial device to our device */
+    Chardev *serial_dev = serial_hd(0);
+    qdev_prop_set_chr(DEVICE(rc), "chardev", serial_dev);
+    
+    /* Create MMIO USART1 Region */
+    memory_region_init_io(&rc->usart1_mmio, OBJECT(stm32l152_obj), &usart1_mops, rc, "USART1", STM32L152_USART1_SIZE);
+    memory_region_add_subregion(get_system_memory(), STM32L152_USART1_BASE, &rc->usart1_mmio);
+}
+
+static void stm32l152_usart1_realize(DeviceState *dev, Error **errp) {
+    Stm32l152Usart1State* rc = STM32L152_USART1(dev);
+
+    /* Initialize GPIO-B MMIO. Currently unimplemented TODO: implement it */
+    if (!memory_region_init_ram(&rc->gpiob_mmio, OBJECT(dev), "GPIOB", STM32L152_GPIOB_SIZE, &error_fatal)) {
+        return;
+    }
+    memory_region_add_subregion(get_system_memory(), STM32L152_GPIOB_BASE, &rc->gpiob_mmio);
+
+    qemu_chr_fe_set_handlers(&rc->char_backend, stm32l152_usart1_can_receive,
+                             stm32l152_usart1_receive, NULL, NULL,
+                             rc, NULL, true);
+}
+
+static Property stm32l152_usart1_properties [] = {
+    DEFINE_PROP_CHR("chardev", Stm32l152Usart1State, char_backend),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void stm32l152_usart1_class_init(ObjectClass *klass, void *data)
+{
+    SysBusDeviceClass* sysclass = SYS_BUS_DEVICE_CLASS(klass);
+    sysclass->parent_class.realize = &stm32l152_usart1_realize;
+    device_class_set_props(DEVICE_CLASS(klass), stm32l152_usart1_properties);
+}
+
+static const TypeInfo stm32l152_usart1_types[] = {
+    {
+        .name           = TYPE_STM32L152_USART1,
+        .parent         = TYPE_SYS_BUS_DEVICE,
+        .instance_size  = sizeof(Stm32l152Usart1State),
+        .instance_init  = stm32l152_usart1_init,
+        .class_init     = stm32l152_usart1_class_init,
+    }
+};
+
+DEFINE_TYPES(stm32l152_usart1_types)
---
+++ include/hw/char/stm32l152_usart.h
@@ -0,0 +1,51 @@
+/*
+ * STM32L152 USART
+ *
+ * Copyright (c) 2025 Guy Sudai
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ * The reference used is the STMicroElectronics RM0351 Reference manual
+ * for STM32L151xx and STM32L152xx advanced Arm ® -based 32-bit MCUs.
+ *
+ * Inspired by the STM32L4x5 by Aranaud Minier
+ */
+
+#ifndef HW_STM32L152_USART_H
+#define HW_STM32L152_USART_H
+
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "qemu/typedefs.h"
+#include "qom/object.h"
+#include "chardev/char-fe.h"
+
+#define TYPE_STM32L152_USART1 "stm32l152-usart1"
+OBJECT_DECLARE_SIMPLE_TYPE(Stm32l152Usart1State, STM32L152_USART1)
+
+struct Stm32l152Usart1State {
+    SysBusDevice parent_obj;
+
+    CharBackend char_backend;
+
+    MemoryRegion usart1_mmio;
+    MemoryRegion gpiob_mmio;
+
+    struct __attribute__((packed)) {
+        uint32_t sr;
+        uint32_t dr;  /* data register - used for TX and RX */
+        uint32_t brr;
+        uint32_t cr1;
+        uint32_t cr2;
+        uint32_t cr3;
+        uint32_t gtpr;
+    } state;
+
+    qemu_irq usart_irq;
+};
+
+
+#endif /* HW_STM32L152_USART_H */
